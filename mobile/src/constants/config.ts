a7b5b0f73b0db83fe648a07ad7ff1d3291605a61
hfreshnel <mahouna-wilfried.houenou@groupe-esigelec.org>
2026-02-02 17:14:42 +0100

    Add icon and splash icon

--- mobile/src/constants/config.ts
+++ mobile/src/constants/config.ts
@@ -1,5 +1,5 @@
-//export const API_URL = "https://back-production-f733.up.railway.app";
-export const API_URL = "http://192.168.1.76:3000/api/v2";
+export const API_URL = "https://backend-production-ec17.up.railway.app/api/v2";
+//export const API_URL = "http://192.168.1.76:3000/api/v2";
 //export const API_URL = "http://localhost:3000/api/v2";
 
 export const IS_DEV = __DEV__;
